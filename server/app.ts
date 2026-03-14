fcde113e97f99b1c553c45f5a44ec765ad5ffa79
Tanishka Khandelwal <tashukhandelwal27@gmail.com>
2026-03-14 21:33:10 +0530

    feat: establish core Express application with middleware, logging, authentication, and routing.

--- server/app.ts
+++ server/app.ts
@@ -15,13 +15,14 @@ declare module "http" {
 
 app.use(
     express.json({
+        limit: "50mb",
         verify: (req: any, _res, buf) => {
             req.rawBody = buf;
         },
     }),
 );
 
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ limit: "50mb", extended: false }));
 
 // IMPORTANT FOR VERCEL
 // Allows express-session secure cookies and passport's correct callbackUrl protocol (HTTPS)
